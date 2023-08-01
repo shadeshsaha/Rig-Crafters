8a547a7766b891ed99ecaf501d9cc393eac664c3
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-08-02 03:50:14 +0600

    Added Navbar & Footer

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import Footer from "@/shared/Footer";
+import Navbar from "@/shared/Navbar";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
